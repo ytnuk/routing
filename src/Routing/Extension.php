d06a0f47aaa1ef4a5bd4e171591cb7904e530281
Vít Kutný <vitkutny@outlook.com>
2014-09-02 22:20:53 +0200

    dev

--- src/Routing/Extension.php
+++ src/Routing/Extension.php
@@ -5,31 +5,10 @@ namespace WebEdit\Routing;
 use WebEdit\Bootstrap;
 use WebEdit\Routing;
 
-final class Extension extends Bootstrap\Extension {
-
-    private $resources = [
-        'routes' => [
-            '[<locale [a-z]{2}(_[A-Z]{2})?>/]<module [a-z.]+>[/<action [a-z]+>][/<id [0-9]+>]' => [
-                'module' => 'Home:Front',
-                'presenter' => 'Presenter',
-                'action' => 'view'
-            ]
-        ]
-    ];
+final class Extension extends Bootstrap\Extension implements Routing\Provider {
 
     public function beforeCompile() {
-        $this->loadResources();
         $this->setupRoutes();
-    }
-
-    private function loadResources() {
-        $this->resources = $this->getConfig($this->resources);
-        foreach ($this->compiler->getExtensions() as $extension) {
-            if (!$extension instanceof Routing\Provider) {
-                continue;
-            }
-            $this->resources = array_merge_recursive($this->resources, $extension->getRoutingResources());
-        }
     }
 
     private function setupRoutes() {
@@ -38,6 +17,12 @@ final class Extension extends Bootstrap\Extension {
         foreach (array_reverse($this->resources['routes']) as $mask => $metadata) {
             $router->addSetup('$service[] = new ' . Routing\Route::class . '(?, ?)', [$mask, $metadata]);
         }
+    }
+
+    public function getRoutingResources() {
+        return [
+            'routes' => []
+        ];
     }
 
 }
