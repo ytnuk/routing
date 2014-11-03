237d4242b4c444fe9f7c5a4c940ac074b4b8b7ed
Vít Kutný <vitkutny@outlook.com>
2014-11-03 09:04:06 +0100

    dev

--- src/Routing/Extension.php
+++ src/Routing/Extension.php
@@ -2,6 +2,8 @@
 
 namespace WebEdit\Routing;
 
+use Nette\Application;
+use Nette\Bridges;
 use Nette\DI;
 
 /**
@@ -9,18 +11,27 @@ use Nette\DI;
  *
  * @package WebEdit\Routing
  */
-final class Extension extends DI\CompilerExtension
+final class Extension extends Bridges\ApplicationDI\RoutingExtension
 {
-
-	private $defaults = [
-		'routes' => []
-	];
 
 	public function beforeCompile()
 	{
 		$config = $this->getConfig($this->defaults);
+		if ($config['debugger']) {
+			$this->getContainerBuilder()
+				->getDefinition('application')
+				->addSetup('@Tracy\Bar::addPanel', [
+					new DI\Statement(Bridges\ApplicationTracy\RoutingPanel::class)
+				]);
+		}
+	}
+
+	public function loadConfiguration()
+	{
+		$config = $this->getConfig($this->defaults);
 		$this->getContainerBuilder()
-			->getDefinition('router')
+			->addDefinition('router')
+			->setClass('Nette\Application\IRouter')
 			->setFactory(Route\Collection::class)
 			->addSetup('addRoutes', [$config['routes']]);
 	}
